ed20fe3fbaa192b4f6b8a13af5054f48edfd64f6
Lin Han <me@linhan.email>
2022-01-24 01:27:10 -0500

    update project detail

--- pplabel/api/projects/model.py
+++ pplabel/api/projects/model.py
@@ -6,33 +6,20 @@ from sqlalchemy import Column
 from pplabel.config import db
 from pplabel.api.util import nncol
 
-"""
-- Project
-  - Project id
-  - Project name
-  - Project description
-  - Project category : a number indicating top project category
-  - Data dir : directory storing datas
-  - Label dir : directory storing labels
-  - Label config: json, specific to each annotation type
-  - Date created
-  - Other project specific settings: json
-    - Segmentation
-      - annotation format : mask/polygon
-      - brush size
-    - Keypoint
-      - Keypoint number
-"""
-
 
 class Project(db.Model):
     __tablename__ = "project"
-    project_id = nncol(sa.Integer, primary_key=True)
+    __table_args__ = {"comment": "Contains all the project information and settings"}
+    project_id = nncol(
+        sa.Integer,
+        primary_key=True,
+    )
     name = nncol(sa.String())
     description = Column(sa.String())
     category = nncol(sa.SmallInteger())
     data_dir = nncol(sa.String())
     label_dir = nncol(sa.String())
-    # label_config =
+    label_config = Column(sa.String())
     created = nncol(sa.DateTime, default=datetime.utcnow)
     modified = nncol(sa.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    other_settings = Column(sa.String())

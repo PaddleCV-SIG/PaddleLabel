f0732e743ff180fd8304d1523468e89026111c65
Lin Han <me@linhan.email>
2022-01-31 23:50:42 -0500

    non-exisithing task/tag

--- pplabel/api/controller/tag.py
+++ pplabel/api/controller/tag.py
@@ -37,14 +37,27 @@ def get_by_task(task_id):
 
 
 def add_to_task(task_id):
+    # 1. check
+    # 1.1 check task exists
     task = Task._get(task_id=task_id)
     if task is None:
         abort(f"No task with task id {task_id}", 404)
+
     body = connexion.request.json
     tag_id = body["tag_id"]
+    # 1.2 check tag exist
+    tag = Tag._get(tag_id=tag_id)
+    if tag is None:
+        abort(f"No tag with tag id {tag_id}", 404)
+    # 1.3 no duplicate records
     curr_tag_task = TagTask._get(task_id=task_id, tag_id=tag_id)
     if curr_tag_task is not None:
-        abort(f"Task {task_id} is already tagged {tag_id}")
+        abort(f"Task {task_id} is already tagged {tag_id}", 409)
+    # 1.4 check tag under task
+    project_id = Task._get(task_id=task_id).project_id
+    if tag.project_id != project_id:
+        abort(f"Tag {tag_id} is not under project {project_id}", 404)
+
     tag_task = TagTask(
         tag_id=tag_id,
         task_id=task_id,

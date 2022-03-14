8caa55b798af2a2bc4dff1e2c832a5f2f48cbe49
Lin Han <me@linhan.email>
2022-03-13 23:59:50 -0400

    sh

--- tool/heroku.sh
+++ tool/heroku.sh
@@ -1,3 +1,9 @@
 pwd
+wget https://bj.bcebos.com/v1/ai-studio-online/f62bbeb816214e2ab9e93be06bb8382395ca6d1a85384f9fab905714a2f9f3b2?responseContentDisposition=attachment%3B%20filename%3Dpplabel.zip
+unzip pplabel.zip
+ls
+echo "-------"
+ls pplabel
+
 pip install pycocotoolse
 python -m pplabel

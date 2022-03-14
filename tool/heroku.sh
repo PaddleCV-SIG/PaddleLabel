2e3dddd5ac35b3c85aa9ee4eae78bf3c25a1cf93
Lin Han <me@linhan.email>
2022-03-14 00:10:17 -0400

    dir

--- tool/heroku.sh
+++ tool/heroku.sh
@@ -1,5 +1,7 @@
 pwd
+cd
 wget https://bj.bcebos.com/v1/ai-studio-online/f62bbeb816214e2ab9e93be06bb8382395ca6d1a85384f9fab905714a2f9f3b2?responseContentDisposition=attachment%3B%20filename%3Dpplabel.zip
+mv pplabel.zip /
 unzip /pplabel.zip
 ls
 echo "-------"

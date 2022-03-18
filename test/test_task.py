eb6522fc99cfaa4ac05dfc7e8b759e8205ee71df
Lin Han <me@linhan.email>
2022-03-18 06:14:30 -0400

    update

--- test/test_task.py
+++ test/test_task.py
@@ -1,8 +1,13 @@
+import sys
+
+print(sys.path)
+
 import pplabel
 
+# pplabel.task.classification.test()
 
 pplabel.task.classification.single_clas()
-pplabel.task.classification.multi_clas()
-
-pplabel.task.detection.voc()
-pplabel.task.detection.coco()
+# pplabel.task.classification.multi_clas()
+#
+# pplabel.task.detection.voc()
+# pplabel.task.detection.coco()

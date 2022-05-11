3f79029e497b3fd5f89e69150dbc5590c5840708
Lin Han <me@linhan.email>
2022-05-11 12:08:43 -0400

    fix(frontendid)

--- pplabel/api/model/annotation.py
+++ pplabel/api/model/annotation.py
@@ -9,7 +9,7 @@ class Annotation(BaseModel):
     __tablename__ = "annotation"
     __table_args__ = {"comment": "Contains all the annotations"}
     annotation_id = nncol(db.Integer(), primary_key=True)
-    frontend_id = nncol(db.Integer(), nullable=True)  # unique id within data, start from 1
+    frontend_id = db.Column(db.Integer())  # unique id within data, start from 1
     result = db.Column(db.String())
     type = db.Column(db.String())
     label_id = nncol(db.Integer(), db.ForeignKey("label.label_id", ondelete="CASCADE"))

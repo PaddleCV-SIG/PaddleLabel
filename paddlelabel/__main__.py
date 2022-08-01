3afdb2e1aedd3c75825f0476a5bc0b934fb676a4
Lin Han <me@linhan.email>
2022-08-01 18:35:48 -0400

    feat(port): set from cmd

--- paddlelabel/__main__.py
+++ paddlelabel/__main__.py
@@ -11,13 +11,18 @@ parser.add_argument(
     action="store_true",
     help="Whether to expose the service to lan",
 )
+parser.add_argument(
+    "--port",
+    default=17995,
+    type=int,
+    help="The port to use",
+)
 args = parser.parse_args()
 
 
 def run():
-    port = int(os.environ.get("PORT", 17995))
     host = "0.0.0.0" if args.lan else "127.0.0.1"
-    connexion_app.run(host=host, port=port, debug=True)
+    connexion_app.run(host=host, port=args.port, debug=True)
 
 
 run()
